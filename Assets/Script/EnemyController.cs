ef2226cf91e44a8440f0c87a70aee1c805557b18
syun39 <134475243+syun39@users.noreply.github.com>
2024-01-31 23:35:18 +0900

    [update] カメラの修正と敵がある程度まで来ると消えるようにした。

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] float _movementSpeed = 5.0f; //ˆÚ“®‘¬“x
+
+    void Update()
+    {
+        transform.Translate(0, 0, _movementSpeed);
+
+        if (transform.position.z < -1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
