3b16725a730aa32f8390a89072035a9d328e18dd
syun39 <134475243+syun39@users.noreply.github.com>
2024-02-07 12:30:28 +0900

    [update]敵を

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] float _movementSpeed = 5.0f; //ˆÚ“®‘¬“x
+    [SerializeField] string _destroyTag = "Destroy";
 
     void Update()
     {
         transform.Translate(0, 0, _movementSpeed);
+    }
 
-        if (transform.position.z < 0)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == _destroyTag)
         {
-            Destroy(gameObject);
+            Destroy(gameObject) ;
         }
     }
 }
